95eb89e8102b1fbf83fc083d1d6aa8f9d4562690
quephird <danielle_kefford@hotmail.com>
2023-07-05 14:28:45 -0700

    Am now able to read and write files as well as transform the zip code.

--- src/truss_interview/core.clj
+++ src/truss_interview/core.clj
@@ -1,5 +1,32 @@
-(ns truss-interview.core)
+(ns truss-interview.core
+  (:require [clojure.data.csv :as csv]
+            [clojure.java.io :as io]))
 
-(defn -main [& args]
-  (println "I am here")
-  (println args))
+(defn transform-zip [zip]
+  (format "%05d" (Integer/parseInt zip)))
+
+(defn process-data-line [[timestamp
+                          address
+                          zip
+                          full-name
+                          foo-duration
+                          bar-duration
+                          total-duration
+                          notes :as line]]
+  [timestamp
+   address
+   (transform-zip zip)
+   full-name
+   foo-duration
+   bar-duration
+   total-duration
+   notes])
+
+(defn -main [input-file-name output-file-name]
+  (with-open [reader (io/reader input-file-name)
+              writer (io/writer output-file-name)]
+    (let [[column-headers & data-lines] (csv/read-csv reader)]
+      (csv/write-csv writer [column-headers])
+      (->> data-lines
+           (map process-data-line)
+           (csv/write-csv writer)))))
