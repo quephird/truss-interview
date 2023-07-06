a30e8eb83357fe0ec1bc1b78afcccde4ba74057e
quephird <danielle_kefford@hotmail.com>
2023-07-06 11:04:22 -0700

    Can now parse and transform durations.

--- src/truss_interview/core.clj
+++ src/truss_interview/core.clj
@@ -1,13 +1,24 @@
 (ns truss-interview.core
   (:require [clojure.data.csv :as csv]
             [clojure.java.io :as io]
-            [clojure.string :as str]))
+            [clojure.string :as str])
+  (:import [java.time Duration]))
 
 (defn transform-zip [zip]
   (format "%05d" (Integer/parseInt zip)))
 
 (defn transform-full-name [name]
   (str/upper-case name))
+
+(defn parse-duration [input]
+  (let [[hours minutes seconds millis] (str/split input #"[:.]")]
+    (-> (Duration/ofHours (Integer/parseInt hours))
+        (.plusMinutes (Integer/parseInt minutes))
+        (.plusSeconds (Integer/parseInt seconds))
+        (.plusMillis (Integer/parseInt millis)))))
+
+(defn transform-duration [duration]
+  (.toSeconds duration))
 
 (defn process-data-line [[timestamp
                           address
@@ -16,15 +27,18 @@
                           foo-duration
                           bar-duration
                           total-duration
-                          notes :as line]]
-  [timestamp
-   address
-   (transform-zip zip)
-   (transform-full-name full-name)
-   foo-duration
-   bar-duration
-   total-duration
-   notes])
+                          notes :as data-line]]
+  (let [parsed-foo-duration (parse-duration foo-duration)
+        parsed-bar-duration (parse-duration bar-duration)
+        computed-total-duration (.plus parsed-foo-duration parsed-bar-duration)]
+    [timestamp
+     address
+     (transform-zip zip)
+     (transform-full-name full-name)
+     (transform-duration parsed-foo-duration)
+     (transform-duration parsed-bar-duration)
+     (transform-duration computed-total-duration)
+     notes]))
 
 (defn -main [input-file-name output-file-name]
   (with-open [reader (io/reader input-file-name)
